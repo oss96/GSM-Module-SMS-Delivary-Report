43818171db4b6193fa6308282e9444e5aa1e69f4
al-alali <al-alali@blueworld-gmbh.de>
2018-12-27 17:21:51 +0100

    Initial Commit

---
+++ GSM Modem/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace GSM_Modem {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.listViewSMS = new System.Windows.Forms.ListView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.comboBoxComPort = new System.Windows.Forms.ComboBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.columnHeaderPhoneNumber = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderStatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderResponse = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnRefreshPorts = new System.Windows.Forms.Button();
+            this.columnHeaderTimeStamp = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderDischarge = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewSMS
+            // 
+            this.listViewSMS.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listViewSMS.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderPhoneNumber,
+            this.columnHeaderStatus,
+            this.columnHeaderTimeStamp,
+            this.columnHeaderDischarge,
+            this.columnHeaderResponse});
+            this.listViewSMS.Location = new System.Drawing.Point(12, 56);
+            this.listViewSMS.Name = "listViewSMS";
+            this.listViewSMS.Size = new System.Drawing.Size(936, 382);
+            this.listViewSMS.TabIndex = 0;
+            this.listViewSMS.UseCompatibleStateImageBehavior = false;
+            this.listViewSMS.View = System.Windows.Forms.View.Details;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(960, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(12, 27);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // comboBoxComPort
+            // 
+            this.comboBoxComPort.FormattingEnabled = true;
+            this.comboBoxComPort.Location = new System.Drawing.Point(93, 28);
+            this.comboBoxComPort.Name = "comboBoxComPort";
+            this.comboBoxComPort.Size = new System.Drawing.Size(198, 21);
+            this.comboBoxComPort.TabIndex = 3;
+            this.comboBoxComPort.SelectedValueChanged += new System.EventHandler(this.comboBoxPorts_SelectedValueChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(454, 29);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // columnHeaderPhoneNumber
+            // 
+            this.columnHeaderPhoneNumber.Text = "Phone Number";
+            this.columnHeaderPhoneNumber.Width = 100;
+            // 
+            // columnHeaderStatus
+            // 
+            this.columnHeaderStatus.Text = "Status";
+            this.columnHeaderStatus.Width = 100;
+            // 
+            // columnHeaderResponse
+            // 
+            this.columnHeaderResponse.Text = "Response";
+            this.columnHeaderResponse.Width = 300;
+            // 
+            // btnRefreshPorts
+            // 
+            this.btnRefreshPorts.Location = new System.Drawing.Point(297, 27);
+            this.btnRefreshPorts.Name = "btnRefreshPorts";
+            this.btnRefreshPorts.Size = new System.Drawing.Size(75, 23);
+            this.btnRefreshPorts.TabIndex = 5;
+            this.btnRefreshPorts.Text = "Refresh";
+            this.btnRefreshPorts.UseVisualStyleBackColor = true;
+            this.btnRefreshPorts.Click += new System.EventHandler(this.btnRefreshPorts_Click);
+            // 
+            // columnHeaderTimeStamp
+            // 
+            this.columnHeaderTimeStamp.Text = "Time Stamp";
+            this.columnHeaderTimeStamp.Width = 200;
+            // 
+            // columnHeaderDischarge
+            // 
+            this.columnHeaderDischarge.Text = "Discharge";
+            this.columnHeaderDischarge.Width = 200;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(960, 450);
+            this.Controls.Add(this.btnRefreshPorts);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.comboBoxComPort);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.listViewSMS);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewSMS;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ComboBox comboBoxComPort;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.ColumnHeader columnHeaderPhoneNumber;
+        private System.Windows.Forms.ColumnHeader columnHeaderStatus;
+        private System.Windows.Forms.ColumnHeader columnHeaderResponse;
+        private System.Windows.Forms.Button btnRefreshPorts;
+        private System.Windows.Forms.ColumnHeader columnHeaderTimeStamp;
+        private System.Windows.Forms.ColumnHeader columnHeaderDischarge;
+    }
+}
+
---
+++ GSM Modem/Form1.cs
@@ -0,0 +1,182 @@
+﻿using GsmComm;
+using GsmComm.PduConverter;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GSM_Modem {
+    public partial class Form1 : Form {
+        private SerialPort port;
+        private string portName;
+
+        public Form1() {
+            InitializeComponent();
+
+            portName = string.Empty;
+            port = null;
+        }
+
+        private bool ConnectSerialPort(string portName) {
+            try {
+                if (portName != string.Empty) {
+                    port = new SerialPort(portName, 115200, Parity.None, 8, StopBits.One);
+                    port.DataReceived += Port_DataReceived;
+                    port.ErrorReceived += Port_ErrorReceived;
+                    port.PinChanged += Port_PinChanged;
+                    port.NewLine = Environment.NewLine;
+                    port.Encoding = Encoding.ASCII;
+
+                    try {
+                        port.Open();
+
+                    } catch (UnauthorizedAccessException) {
+                        // ignore exception
+                        System.Media.SystemSounds.Asterisk.Play();
+                    }
+
+                } else {
+                    MessageBox.Show("Port Name not set", "ERROR!");
+                    return false;
+                }
+
+            } catch (IOException ioe) {
+                CloseSerialPort();
+                MessageBox.Show(ioe.Message, "ERROR!");
+                return false;
+            } catch (Exception e) {
+                CloseSerialPort();
+                MessageBox.Show(e.Message, "ERROR!");
+                return false;
+            }
+
+            if (btnConnect.Text == "Connect") {
+                btnConnect.Text = "Disconnect";
+            }
+
+            return true;
+        }
+
+        protected override void OnLoad(EventArgs e) {
+            base.OnLoad(e);
+
+            var ports = SerialPort.GetPortNames();
+            comboBoxComPort.DataSource = ports;
+        }
+
+        protected override void OnClosing(CancelEventArgs e) {
+            CloseSerialPort();
+
+            base.OnClosing(e);
+        }
+
+        private void comboBoxPorts_SelectedValueChanged(object sender, EventArgs e) {
+            var cb = sender as ComboBox;
+            portName = cb.SelectedValue.ToString();
+        }
+
+        private void CloseSerialPort() {
+            if (port != null) {
+                if (port.IsOpen) {
+                    try {
+                        port.Close();
+
+                    } catch (IOException e) {
+                        // ignore, and just close
+                    }
+                }
+                port = null;
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e) {
+            if (btnConnect.Text == "Connect") {
+                if (ConnectSerialPort(portName)) {
+                    btnConnect.Text = "Disconnect";
+                    comboBoxComPort.Enabled = false;
+                    btnRefreshPorts.Enabled = false;
+                } else {
+                    System.Media.SystemSounds.Asterisk.Play();
+                }
+
+            } else {
+                CloseSerialPort();
+                btnConnect.Text = "Connect";
+                comboBoxComPort.Enabled = true;
+                btnRefreshPorts.Enabled = true;
+            }
+        }
+
+        private void SendCommand(string command) {
+            if (port == null) {
+                if (!ConnectSerialPort(portName)) {
+                    return;
+                }
+            }
+
+            if (!port.IsOpen) {
+                port.Open();
+            }
+
+            port.WriteLine(command);
+        }
+
+        private void Port_ErrorReceived(object sender, SerialErrorReceivedEventArgs e) {
+            Debug.WriteLine(e.EventType.ToString() + ": " + e.ToString());
+        }
+
+        private void Port_DataReceived(object sender, SerialDataReceivedEventArgs e) {
+            Debug.WriteLine(e.EventType.ToString() + ": " + e.ToString());
+            string res = (sender as SerialPort).ReadExisting();
+            ParseSmsResponse(res);
+        }
+
+        private void Port_PinChanged(object sender, SerialPinChangedEventArgs e) {
+            Debug.WriteLine(e.EventType.ToString() + ": " + e.ToString());
+        }
+
+        private void btnRefreshPorts_Click(object sender, EventArgs e) {
+            var ports = SerialPort.GetPortNames();
+            comboBoxComPort.DataSource = ports;
+        }
+
+
+        private void ParseSmsResponse(string response) {
+            if (response.ToLower().Contains("cmgl")) {
+                string[] allResponses = response.Split(new string[] { "+CMGL:" }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 1; i < allResponses.Length; i++) {
+                    string[] parts = allResponses[i].Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (parts.Length > 1) {
+                        SmsStatusReportPdu smsInfo = (SmsStatusReportPdu)IncomingSmsPdu.Decode(parts[1], true);
+                        string[] listItem = new string[] {
+                            smsInfo.RecipientAddress,
+                            smsInfo.Status.Category.ToString(),
+                            smsInfo.SCTimestamp.ToDateTime().ToString(),
+                            smsInfo.DischargeTime.ToDateTime().ToString(),
+                            parts[1]
+                        };
+
+                        listViewSMS.Invoke((MethodInvoker)delegate {
+                            listViewSMS.Items.Add(new ListViewItem(listItem));
+                        });
+                    }
+                }
+
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e) {
+            SendCommand("at+cmgl=4");
+        }
+    }
+}
